eea4b53e07b6b401cfdb33995bfd73f04f127c1b
phankandy <whota0@163.com>
2025-07-25 23:44:23 +0800

    fix(wsl): code-wrapper 封装不对

--- shell/wsl/code-open-wrapper.sh
+++ shell/wsl/code-open-wrapper.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# 没有参数时直接调用原始code命令
-if [ $# -eq 0 ]; then
-  code
-  exit 0
-fi
+function _code_open_wrapper {
+  # 没有参数时直接调用原始code命令
+  if [ $# -eq 0 ]; then
+    code
+    exit 0
+  fi
 
-full_path=$(realpath "$1")
-distro_name=${WSL_DISTRO_NAME:-FedoraLinux-42}
+  local full_path=$(realpath "$1")
+  local distro_name=${WSL_DISTRO_NAME:-FedoraLinux-42}
 
-# 检查第一个参数是否是目录
-if [ -d "$1" ]; then
-  code --folder-uri "vscode-remote://wsl+${distro_name}${full_path}"
-else
-  code --file-uri "vscode-remote://wsl+${distro_name}${full_path}"
-  code "$@"
-fi
+  # 检查第一个参数是否是目录
+  if [ -d "$1" ]; then
+    code --folder-uri "vscode-remote://wsl+${distro_name}${full_path}"
+  else
+    code --file-uri "vscode-remote://wsl+${distro_name}${full_path}"
+    code "$@"
+  fi
+}
 
-alias code="$(dirname $(readlink -f "$0"))/$0"
+
+alias code=_code_open_wrapper
