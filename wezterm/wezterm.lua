a4d08d061a1d64102de42e879121f9637e7244d6
phankandy <whota0@163.com>
2025-08-22 18:36:30 +0800

    wezterm: simplify config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,98 +1,47 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
 
-local launch_menu = {}
-local default_prog = {}
-
--- 设置SHELL终端
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	-- windows下启用powershell
-	local powershell_path = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
-	local pwsh_path = "C:/Program Files/PowerShell/7/pwsh.exe"
-	--term = '' Set to empty so FZF works on windows
-	table.insert(launch_menu, {
-		label = "PowerShell",
-		args = { pwsh_path, "-NoLogo" },
-	})
-	default_prog = { pwsh_path, "-NoLogo" }
-end
-
+-- Appearance
+config.window_padding = { left = 10, right = 10, top = 10, bottom = 0 }
+config.initial_rows = 40
+config.initial_cols = 150
+-- config.window_background_opacity = 1.0 -- 背景不透明度
+config.color_scheme = "Catppuccin Mocha" -- 设置主题
+config.window_decorations = "RESIZE"
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-
-	-- 设置初始位置
-	window:gui_window():set_position(200, 150)
-
-	-- 默认窗口最大化
-	-- window:gui_window():maximize()
+	window:gui_window():set_position(200, 150) -- 设置初始位置
 end)
 
-config.font = wezterm.font_with_fallback({ "CodeNewRoman Nerd Font Mono", "LXGW WenKai" }) --设置默认字体
-
-config.font_size = 12
-
-config.default_prog = default_prog
-
---- Tab Bar Config ---
+-- Tab
+config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_max_width = 50
 config.show_tab_index_in_tab_bar = true
--- hide_tab_bar_if_only_one_tab = true
 -- config.enable_tab_bar = false -- 关闭 tab_bar
 
-config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 10,
-	bottom = 0,
-}
-
--- 初始窗口大小
-config.initial_rows = 40
-config.initial_cols = 150
+-- Font
+config.font = wezterm.font_with_fallback({ "CodeNewRoman Nerd Font Mono", "LXGW WenKai" }) --设置默认字体
+config.font_size = 12
 config.adjust_window_size_when_changing_font_size = false
 
-config.window_background_opacity = 1.0   -- 背景不透明度
-
-config.color_scheme = "Catppuccin Mocha" -- 设置主题
-config.window_decorations = "RESIZE"
-config.show_new_tab_button_in_tab_bar = false
-
+-- Other
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
+config.default_prog = wezterm.target_triple == "x86_64-pc-windows-msvc" and { "wsl.exe", "~" } or nil
 
-wezterm.on("update-status", function(window)
-	-- Grab the utf8 character for the "powerline" left facing
-	-- solid arrow.
-	-- local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-	local LEFT_ARROW = utf8.char(0xe0b3)
-
-	-- Grab the current window's configuration, and from it the
-	-- palette (this is the combination of your chosen colour scheme
-	-- including any overrides).
-	local color_scheme = window:effective_config().resolved_palette
-	local bg = color_scheme.background
-	local fg = color_scheme.foreground
-
-	window:set_right_status(wezterm.format({
-		-- -- First, we draw the arrow...
-		-- { Background = { Color = bg } },
-		-- { Foreground = { Color = fg } },
-		-- { Text = LEFT_ARROW },
-		-- Then we draw our text
-		-- { Background = { Color = bg } },
-		-- { Foreground = { Color = fg } },
-		-- { Text = " " .. wezterm.hostname() .. " " },
-	}))
-end)
-
--- 快捷键
-config.keys = require("keymaping").keys
-
-config.mouse_bindings = require("keymaping").mouse_bindings
-
--- config.enable_ssh_agent = false
+-- Keymapping
+config.keys = {
+	{ key = "z", mods = "ALT", action = wezterm.action.ShowLauncher },
+	{ key = "a", mods = "CTRL|SHIFT", action = wezterm.action.ShowTabNavigator },
+	{ key = "6", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "_", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
+	{ mods = "", key = "Home", action = wezterm.action({ SendString = "\001" }) },
+	{ mods = "", key = "End", action = wezterm.action({ SendString = "\005" }) },
+}
+config.mouse_bindings = {
+	-- Ctrl-click will open the link under the mouse cursor
+	{ event = { Up = { streak = 1, button = "Left" } }, mods = "CTRL", action = wezterm.action.OpenLinkAtMouseCursor },
+}
 
 return config
-
--- vim: ts=2 sts=2 sw=2 et
