192d14ebd83233cb2dd00fa28a4e9a0969361fe0
phankandy <whota0@163.com>
2025-08-22 14:06:43 +0800

    tmux: update clipboard

--- tmux/simple.conf.sh
+++ tmux/simple.conf.sh
@@ -5,7 +5,7 @@ set -a terminal-features 'xterm-256color:RGB'
 set -g base-index 1
 set -g pane-base-index 1
 set -g renumber-windows on
-set -s set-clipboard on # Support OSC52
+set -s set-clipboard external # Support OSC52
 set -g status-style "bg=default"
 set -g window-status-current-style "fg=blue bold"
 set -g status-right ""
