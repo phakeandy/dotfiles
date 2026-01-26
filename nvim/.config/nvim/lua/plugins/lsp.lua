b4dc964caa09c54d23ed6e28b365a4362c7ddf49
phakeandy <whota0@163.com>
2026-01-26 13:07:46 +0800

    update

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,11 @@
 vim.pack.add({
-  { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
 })
 
 require("mason").setup()
 
-vim.lsp.enable({ "lua_ls", "clangd", "vtsls", "html-lsp", "gopls" })
+vim.lsp.enable({ "lua_ls", "clangd", "vtsls", "html-lsp", "gopls", "yaml", "pyright" })
 vim.lsp.inlay_hint.enable(true)
 
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
