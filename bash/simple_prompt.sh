22ec09de88e858c5277b10b1a571ac5652a1655c
phakeandy <whota0@163.com>
2026-06-14 11:51:39 +0800

    update

--- bash/simple_prompt.sh
+++ bash/simple_prompt.sh
@@ -9,9 +9,9 @@ _update_bash_prompt() {
     local blue='\[\033[01;34m\]'
     local red='\[\033[01;31m\]'
     local reset='\[\033[00m\]'
-    # local title='\[\e]0;\u: \w\a\]'
+    local purple='\[\033[38;5;62m\]'
 
-    PS1="${title}${debian_chroot:+($debian_chroot)}${green}${job_str}${reset}\$ "
+    PS1="${title}${debian_chroot:+($debian_chroot)}${green}${job_str}${reset}${purple}\$${reset} "
 }
 
 PROMPT_COMMAND="_update_bash_prompt"
